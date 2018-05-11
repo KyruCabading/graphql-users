0a48cd7a1f21951550b0afa39f7f465236ee2ecd
Kyru Cabading <kyrusri@gmail.com>
2018-05-11 22:47:48 +0800

    add external db & axios

--- schema/schema.js
+++ schema/schema.js
@@ -1,5 +1,5 @@
 const graphql = require('graphql');
-const _ = require('lodash');
+const axios = require('axios');
 const {
   GraphQLObjectType,
   GraphQLString,
@@ -7,11 +7,7 @@ const {
   GraphQLSchema
 } = graphql;
 
-const users = [
-  { id: '23', firstName: 'Baby', age: 20 },
-  { id: '3', firstName: 'Kyru', age: 19 },
-  { id: '20', firstName: 'Raya', age: 18 },
-]
+
 const UserType = new GraphQLObjectType({
   name: 'User',
   fields: {
@@ -28,7 +24,8 @@ const RootQuery = new GraphQLObjectType({
       type: UserType,
       args: { id: { type: GraphQLString } },
       resolve(parentValue, args) {
-        return _.find(users, { id: args.id });
+        return axios.get(`http://localhost:3000/users/${args.id}`)
+          .then(res => res.data);
       }
     }
   }
